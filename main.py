b058c04b658abab90023c0a9acb97aa4972f5122
Cezary Maćkowski <cezary.mackowski@iteo.com>
2022-03-30 19:38:46 +0200

    early version

--- main.py
+++ main.py
@@ -1,28 +1,61 @@
 import argparse
 import json
+import signal
 import sys
 from typing import Any
 import csv
+import statistics 
 
 import paho.mqtt.client as mqtt
 import paho.mqtt.subscribe as sub
 
+# headers_filtered = ["client_id", "rssi", "txpower", "mac", "x", "y"," ", "Min-RSSI", "Max-RSSI", "Mean-RSSI", "Median-RSSI","TxPower"]
+
+# Globals
+topic = ""
+
+# devices
 headers_devices = ["client_id", "rssi", "txpower", "mac"]
-headers_filtered = ["client_id", "rssi", "txpower", "mac", "x", "y"]
+final_array_devices = []
+
+# filtered
+headers_filtered = ["Min-RSSI", "Max-RSSI", "Mean-RSSI", "Median-RSSI", "TxPower"]
+final_array_filtered = []
+filtered_unique_mac = []
+filtered_temp_array = []
+
+# position
 headers_position = ["client_id", "x", "y", "error", "date"]
+final_array_position = []
 
 
 def input_validation() -> dict:
-    """A function that parses program parameters and checks their correctness"""
+    global topic
     argument_parser = argparse.ArgumentParser()
-    argument_parser.add_argument("-p", "--port", type=int, required=True, help="(Enter the port number)")
-    argument_parser.add_argument("-B", "--bind", type=str, required=True, help="(Enter the address of the broker)")
-    argument_parser.add_argument("-t", "--topic", type=str, required=True, help="(Enter a topic)",
-                                 choices=["devices", "filtered", "position"])
+    argument_parser.add_argument(
+        "-p", "--port", type=int, required=True, help="(Enter the port number)"
+    )
+    argument_parser.add_argument(
+        "-B",
+        "--bind",
+        type=str,
+        required=True,
+        help="(Enter the address of the broker)",
+    )
+    argument_parser.add_argument(
+        "-t",
+        "--topic",
+        type=str,
+        required=True,
+        help="(Enter a topic)",
+        choices=["devices", "filtered", "position"],
+    )
 
     args = vars(argument_parser.parse_args())
+    topic = args["topic"]
 
     return args
+
 
 def on_message_devices(client: mqtt.Client, userdata: Any, message: Any) -> None:
     devices_json = json.loads(message.payload)
@@ -31,10 +64,11 @@ def on_message_devices(client: mqtt.Client, userdata: Any, message: Any) -> None
     temp_dict = {"client_id": devices_json["client_id"]}
 
     for device in devices_json["devices"]:
-        temp_dict2 = {"rssi": device["rssi"],
-                      "txpower": device["txpower"],
-                      "mac": device["mac"]
-                      }
+        temp_dict2 = {
+            "rssi": device["rssi"],
+            "txpower": device["txpower"],
+            "mac": device["mac"],
+        }
         array_devices.append({**temp_dict, **temp_dict2})
 
     with open("./data/devices.csv", "a", encoding="UTF8", newline="") as f:
@@ -45,25 +79,43 @@ def on_message_devices(client: mqtt.Client, userdata: Any, message: Any) -> None
 
 
 def on_message_filtered(client: mqtt.Client, userdata: Any, message: Any) -> None:
-    filtered_json = json.loads(message.payload)
-    array_filtered = []
+    global final_array_filtered
 
-    temp_dict = {"client_id": filtered_json["client_id"]}
+    filtered_json = json.loads(message.payload)
 
     for rpi in filtered_json["rpi"]:
-        temp_dict2 = {"rssi": rpi["rssi"],
-                      "txpower": rpi["txpower"],
-                      "mac": rpi["mac"],
-                      "x": rpi["x"],
-                      "y": rpi["y"]
-                      }
-        array_filtered.append({**temp_dict, **temp_dict2})
+        if rpi["mac"] not in filtered_unique_mac:
+            filtered_unique_mac.append(rpi["mac"])
 
-    with open("./data/filtered.csv", "a", encoding="UTF8", newline="") as f:
-        writer = csv.DictWriter(f, fieldnames=headers_devices)
-        writer.writerows(array_filtered)
+        filtered_temp_array.append({
+            "mac":rpi["mac"], 
+            "rssi": rpi["rssi"], 
+            "txpower": rpi["txpower"]
+        })
+    
 
-    array_filtered.pop()
+    # temp_dict = {"client_id": filtered_json["client_id"]}
+
+    # for rpi in filtered_json["rpi"]:
+
+    #     if rpi["mac"] not in filtered_unique_mac:
+    #         filtered_unique_mac.append(rpi["mac"])
+
+    #     filtered_rssi.append(rpi["rssi"])
+
+    #     temp_dict2 = {"rssi": rpi["rssi"],
+    #                   "txpower": rpi["txpower"],
+    #                   "mac": rpi["mac"],
+    #                   "x": rpi["x"],
+    #                   "y": rpi["y"]
+    #                   }
+    #     array_filtered.append({**temp_dict, **temp_dict2})
+
+    # with open("./data/filtered.csv", "a", encoding="UTF8", newline="") as f:
+    #     writer = csv.DictWriter(f, fieldnames=headers_devices)
+    #     writer.writerows(array_filtered)
+
+    # array_filtered.pop()
 
 
 def on_message_position(client: mqtt.Client, userdata: Any, message: Any) -> None:
@@ -74,11 +126,12 @@ def on_message_position(client: mqtt.Client, userdata: Any, message: Any) -> Non
     temp_dict3 = {"date": position_json["date"]}
 
     for coords in position_json["coordinates"]:
-        temp_dict2 = {"x": coords["rssi"],
-                      "y": coords["txpower"],
-                      "error": coords["mac"]
-                      }
-        array_position.append({**temp_dict, **temp_dict2,**temp_dict3})
+        temp_dict2 = {
+            "x": coords["rssi"],
+            "y": coords["txpower"],
+            "error": coords["mac"],
+        }
+        array_position.append({**temp_dict, **temp_dict2, **temp_dict3})
 
     with open("./data/position.csv", "a", encoding="UTF8", newline="") as f:
         writer = csv.DictWriter(f, fieldnames=headers_devices)
@@ -88,7 +141,6 @@ def on_message_position(client: mqtt.Client, userdata: Any, message: Any) -> Non
 
 
 def set_up_client() -> mqtt.Client:
-    """A function to create and connect a client to the MQTT broker"""
     client = mqtt.Client()
 
     try:
@@ -99,34 +151,66 @@ def set_up_client() -> mqtt.Client:
     return client
 
 
-if __name__ == '__main__':
+def exit_handler(signum, frame):
+    global topic, filtered_unique_mac, filtered_dict_of_everything
+    if topic == "devices":
+
+        with open("./data/devices.csv", "w", encoding="UTF8", newline="") as f:
+            writer = csv.DictWriter(f, fieldnames=headers_devices)
+            writer.writeheader()
+            writer.writerows(final_array_devices)
+    elif topic == "filtered":
+        
+        temp_cos_array = []
+        temp_rssi_array = []
+        for address in filtered_unique_mac:
+            for element in filtered_temp_array:
+                if address == element["mac"]:
+                    temp_rssi_array.append(element["rssi"])
+            temp_cos = {"mac": address,"rssi": temp_rssi_array,"txpower": element["txpower"]}
+            temp_cos_array.append(temp_cos)
+        
+        
+        for element in temp_cos_array:
+            final_array_filtered.append(
+                {
+                    "mac":element["mac"],
+                    "min_rssi": min(element["rssi"]),
+                    "max_rssi": max(element["rssi"]),
+                    "mean_rssi": statistics.mean(element["rssi"]),
+                    "median_rssi": statistics.median(element["rssi"]),
+                    "txpower": element["txpower"]
+                }
+            )
+        print(final_array_filtered)
+        with open("./data/filtered.csv", "a", encoding="UTF8", newline="") as f:
+            writer = csv.DictWriter(f, fieldnames=headers_filtered)
+            writer.writeheader()
+            writer.writerows(final_array_filtered)
+
+
+    else:
+        
+        with open("./data/position.csv", "w", encoding="UTF8", newline="") as f:
+            writer = csv.DictWriter(f, fieldnames=headers_position)
+            writer.writeheader()
+            writer.writerows(final_array_position)
+
+    sys.exit("Poprawnie zamknięto program!")
+
+
+signal.signal(signal.SIGINT, exit_handler)
+
+if __name__ == "__main__":
     args = input_validation()
     client = set_up_client()
 
-    topic = args["topic"]
     if topic == "devices":
-
-        with open("./data/devices.csv", "a", encoding="UTF8", newline="") as f:
-            writer = csv.DictWriter(f, fieldnames=headers_devices)
-            writer.writeheader()
-
         sub.callback(callback=on_message_devices, topics=["devices"], hostname=args["bind"],
                      port=args["port"])
-
     elif topic == "filtered":
-
-        with open("./data/filtered.csv", "a", encoding="UTF8", newline="") as f:
-            writer = csv.DictWriter(f, fieldnames=headers_devices)
-            writer.writeheader()
-
         sub.callback(callback=on_message_filtered, topics=["filtered"], hostname=args["bind"],
                      port=args["port"])
-
     else:
-
-        with open("./data/position.csv", "a", encoding="UTF8", newline="") as f:
-            writer = csv.DictWriter(f, fieldnames=headers_devices)
-            writer.writeheader()
-
         sub.callback(callback=on_message_position, topics=["position"], hostname=args["bind"],
                      port=args["port"])
